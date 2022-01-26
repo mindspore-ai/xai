80ad2de71b68b526ed094665d468b08522f04e58
fujianzhao <fujianzhao@huawei.com>
2022-01-26 15:44:03 +0800

    feat: update patch, lime
    1. cache package zip file in local directory
    2. update patch config file format
    3. LIME support SKlearn model and numpy array
    4. better visualization of LIME explanation
    5. update LIME example notebook.
    6. add LIME tests.

--- scripts/apply_patch.py
+++ scripts/apply_patch.py
@@ -14,11 +14,29 @@
 """Apply patch."""
 import argparse
 import shutil
-import tempfile
 import filecmp
 from pathlib import Path
 
-from utils import get_patch_file, get_package_dir, load_config, git_apply_patch, get_repo_from_url, PACKAGES
+from utils import get_patch_file, get_package_local_dir, load_config, git_apply_patch, get_source_code_from_url, \
+    PACKAGES, cache_dir
+
+
+def safe_copy(src_path, dst_path):
+    """
+    safe copy file
+
+    Args:
+        src_path (Path): Source.
+        dst_path (Path): Destination.
+    """
+    if dst_path.is_file():
+        if not filecmp.cmp(str(src_path), str(dst_path)):
+            raise FileExistsError('Your local changes to the file {} would be overwritten by the patch, '
+                                  'please create a new patch from it or delete the file.'.format(dst_path))
+
+    dst_path.parent.mkdir(exist_ok=True, parents=True)
+    shutil.copyfile(str(src_path), str(dst_path))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='clone open source codes and apply patch')
@@ -30,24 +48,30 @@ if __name__ == "__main__":
 
     url, files = load_config(package)
     # The package location inside the xai, e.g. xai/mindspore_xai/third_party/lime
-    package_dir = get_package_dir(package)
+    local_dir = get_package_local_dir(package)
     patch_file = get_patch_file(package)
 
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        repo_dir = Path(tmp_dir) / package
-        get_repo_from_url(url, repo_dir)
+    source_code_dir = cache_dir / package
 
-        git_apply_patch(str(repo_dir), patch_file)
+    get_source_code_from_url(url, package)
+    git_apply_patch(str(source_code_dir), patch_file)
 
-        # copy the interested files from repo directory to local directory
-        for f in files:
-            src = repo_dir / f['src']
-            dst = package_dir / f['dst']
-            if dst.is_file():
-                if not filecmp.cmp(str(src), str(dst)):
-                    raise FileExistsError('Your local changes to the file {} would be overwritten by the patch, '
-                                          'please create a new patch from it or delete the file.'.format(dst))
-            dst.parent.mkdir(exist_ok=True, parents=True)
-            shutil.copyfile(str(src), str(dst))
+    # copy the interested files from source code directory to package local directory
+    for f in files:
+        source_code_p = source_code_dir / f
+        local_p = local_dir / f
 
-    print('patch applied to {}'.format(package_dir))
+        if source_code_p.is_file():
+            safe_copy(source_code_p, local_p)
+        elif source_code_p.is_dir():
+            for src in source_code_p.rglob("*.py"):
+                dst = str(src).replace(str(source_code_p), str(local_p))
+                safe_copy(src, Path(dst))
+
+    # remove the source codes
+    shutil.rmtree(str(source_code_dir))
+
+    # create __init__.py
+    (local_dir / "__init__.py").touch()
+
+    print('patch applied to {}'.format(local_dir))
