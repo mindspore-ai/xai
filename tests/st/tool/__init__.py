07f578fc4f28a930e7961c824514f0796daf5a07
Ng Ngai Fai <ngngaifai@huawei.com>
2022-03-18 13:37:58 +0800

    TabSim ST

--- tests/st/tool/__init__.py
+++ tests/st/tool/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Initialization of smoke tests."""
+"""Initialization of tests of tools."""
